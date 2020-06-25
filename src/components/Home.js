8478a315e9fbdf1ff9c33d3f2744ae5b94eeaf1c
Denis Posmetnij <depyden@MacBook-Air-Danny.local>
2020-06-25 13:38:52 +0300

    MinorFix

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import './component.css';
+import { Link } from "react-router-dom";
 
-class Home extends React.Component{
-render() {
-    return (
-        <div>
-          <h2>Home</h2>
-        </div>
-    );
-  }
-}
+const Home = () => (
+  <div className="jumbotron">
+    <h1>Open Analyzer</h1>
+    <p>Analyzer from OpenPolyEdu team.</p>
+    <Link to="tech_specifications" className="btn btn-primary btn-lg">
+      Learn more
+    </Link>
+  </div>
+);
 
 export default Home;
--- src/components/Tech_specifications.js
+++ src/components/Tech_specifications.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
 class Specifications extends React.Component{
-render() {
+  render() {
     return (
         <div>
-          <h2>Specifications</h2>
+          <h2>About</h2>
+          <p>
+            This app uses React, React Router, and many other helpful
+            libraries. (README will be available later)
+          </p>
+          <p>This app also uses Antd as a design library. You can check it here:</p>
+          <a href="https://ant.design" target="_blank" rel="noopener noreferrer">Antd Design library</a>
         </div>
     );
   }
