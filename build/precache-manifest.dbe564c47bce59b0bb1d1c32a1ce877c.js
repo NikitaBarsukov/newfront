fb7d620a4d344624676b6be33feda81f0e496b89
Denis Posmetnij <depyden@MacBook-Air-Danny.local>
2021-01-21 01:33:02 +0300

    build

---
+++ build/precache-manifest.dbe564c47bce59b0bb1d1c32a1ce877c.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c5b3e00a2b7cd0bd72f223bab4d434",
+    "url": "/index.html"
+  },
+  {
+    "revision": "adddb6cecfa3225d4360",
+    "url": "/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "b2ca33421e9aaf692db3",
+    "url": "/static/css/main.d9d0857e.chunk.css"
+  },
+  {
+    "revision": "adddb6cecfa3225d4360",
+    "url": "/static/js/2.dacf3950.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.dacf3950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ca33421e9aaf692db3",
+    "url": "/static/js/main.8d711e58.chunk.js"
+  },
+  {
+    "revision": "cc76698a17c92a11c0ed",
+    "url": "/static/js/runtime-main.9da2ca49.js"
+  },
+  {
+    "revision": "f6bce3409f7cdc7b2639979d4e1a1d8f",
+    "url": "/static/media/GHLogo.f6bce340.png"
+  },
+  {
+    "revision": "f128bcfcda19c090cce95e48c208620d",
+    "url": "/static/media/GHPicture1.f128bcfc.jpg"
+  },
+  {
+    "revision": "ad41d3e0f5d000830905d863dba3cadd",
+    "url": "/static/media/GHPicture2.ad41d3e0.jpg"
+  },
+  {
+    "revision": "82167a8edd6daf1b8a8959ab8b40055f",
+    "url": "/static/media/GHPicture3.82167a8e.jpg"
+  },
+  {
+    "revision": "0fc881b2075f03f2d3920c46d3c182e5",
+    "url": "/static/media/GHPicture4.0fc881b2.jpg"
+  },
+  {
+    "revision": "d70bb6b2636bcd784f2611cac933fc83",
+    "url": "/static/media/GHPicture5.d70bb6b2.jpg"
+  },
+  {
+    "revision": "4efd6fa62d3e813a38955b615e9ce9be",
+    "url": "/static/media/GHPicture6.4efd6fa6.jpg"
+  },
+  {
+    "revision": "2413e1db7e097bd6a1b7b07c2351801e",
+    "url": "/static/media/GHPicture7.2413e1db.jpg"
+  }
+]);
