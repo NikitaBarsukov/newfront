3173d1050b525028b4e86957274309cbe5455bfb
Nikita Barsukov <nikbars1997@gmail.com>
2021-01-21 13:56:50 +0300

    added build folder

---
+++ build/precache-manifest.65803f1d7d9bd6445813a2195a6d603b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "546574e886b6de7e7e149d80e27111e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc83df3c97ba218fa7ab",
+    "url": "/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "6580ca11489bd7d77353",
+    "url": "/static/css/main.747fa5a6.chunk.css"
+  },
+  {
+    "revision": "bc83df3c97ba218fa7ab",
+    "url": "/static/js/2.47ae0163.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.47ae0163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6580ca11489bd7d77353",
+    "url": "/static/js/main.4ae85cdf.chunk.js"
+  },
+  {
+    "revision": "baf4de1c12d39bd1cdd1",
+    "url": "/static/js/runtime-main.ef079484.js"
+  }
+]);
